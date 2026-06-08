2467f9fef08226ebc2972f17a72f84a3309f7813
Akshat Saxena <akshatshyamsaxena760@gmail.com>
2026-06-08 16:14:50 +0530

    Added mongo db atlas and started auth

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,13 @@
+exports.register = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Register controller working",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
---
+++ backend/src/routes/testRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.get("/test-user", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
